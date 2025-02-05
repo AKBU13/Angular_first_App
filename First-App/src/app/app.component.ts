998051d419164a1d57084c896d510adb96c4d098
ATIN Krakou <ulrichatin@gmail.com>
2025-02-05 21:31:10 +0000

    Firt using of angular material library

--- First-App/src/app/app.component.ts
+++ First-App/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { MatDividerModule} from '@angular/material/divider';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button'
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
